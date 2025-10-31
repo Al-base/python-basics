3997c0f9ffbd3ede2b028ffe8cd2f2cc8b152bd9
Al-base <ali.deeb.pm@gmail.com>
2025-10-31 01:14:02 +0100

    Day1: environment setup and basic syntax

---
+++ intro.py
@@ -0,0 +1,46 @@
+print("ali")
+Firat_name ="Ali"
+Last_name = "Deeb"
+Full_name = Firat_name +' '+ Last_name
+print("Hello "+ Full_name)
+print(type(Full_name))
+Age = 29
+Age = Age+1
+print(Age)
+Age+=1
+print(Age)
+print(type(Age))
+print("Your Age is = "+ str(Age))
+Height = 176.5
+print(Height)
+print(type(Height))
+print("Your height is :"+ str(Height)+"CM")
+Human = True
+print(Human)
+print(type(Human))
+print("are you a human : "+str(Human))
+Name,age2,Atrractive = "Ali" , 21 , True
+personA = personb =personec = 30
+print(personA)
+print(personb)
+print(personec)
+print(len(Name))
+print(Name.find("l"))
+print(Name.capitalize())
+print(Name.upper())
+print(Name.lower())
+print(Name.isdigit())
+print(Name.isalpha())
+print(Name.count("A"))
+print(Name.replace("A","M"))
+print(Name*3)
+x = 1 #int
+y = 2.0 #float
+z = "3" #str
+z = int(z)
+x = float(x) 
+print(x)
+print(str(y))
+print(z*3)
+name3 = input("What is your name ? ")
+age3 = input("How old are you ? ")
