e2197e15851c8f220298a9aec6bcdcf445554036
Al-base <ali.deeb.pm@gmail.com>
2025-11-11 22:26:04 +0100

    improved user input in intro.py

--- intro.py
+++ intro.py
@@ -1,3 +1,4 @@
+import math
 print("ali")
 Firat_name ="Ali"
 Last_name = "Deeb"
@@ -42,5 +43,58 @@ x = float(x)
 print(x)
 print(str(y))
 print(z*3)
-name3 = input("What is your name ? ")
-age3 = input("How old are you ? ")
+#name3 = input("What is your name ? ")
+#age3 = input("How old are you ? ")
+#Height3 = float(input("What is your height ? "))
+#print("Your height is "+ str(Height3))
+#print("Hello "+ name3 + " Your age is "+ age3)
+pi = 3.14
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(-pi))   
+print(pow(pi,3))
+print(math.sqrt(36))
+A4 = 5
+B4 = 3  
+c4 = A4 + B4
+print(max(A4,c4))
+print(min(A4,B4))
+Unsliced_name = "ALI DEEB"
+First_Sliced_name = Unsliced_name[0:3]
+print(First_Sliced_name)
+Second_Sliced_name = Unsliced_name[4:8]
+print(Second_Sliced_name)
+Third_Sliced_name = Unsliced_name[::2]
+print(Third_Sliced_name)
+reversed_name = Unsliced_name[::-1]
+print(reversed_name)
+Fourth_Sliced_name = Unsliced_name[-4:]
+print(Fourth_Sliced_name)
+Fifth_Sliced_name = Unsliced_name[:-4]
+print(Fifth_Sliced_name)
+website = "http://www.abc.com"
+slice = slice(7,-4)
+sliced_website = website[slice]
+print(sliced_website)
+age4 = int(input("Enter your age : "))
+if age4 >=18 and age4 <100:
+    print("You are adult")  
+elif age4 <0 : 
+    print("Invalid age")
+elif age4 ==100:
+    print("You are centenarian")
+else:
+    print("You are a child")
+temperature = int(input("Enter the temperature : "))
+if temperature >30:
+    print("It's a hot day")
+    print("Drink plenty of water")  
+elif temperature >20 and temperature <=30:
+    print("It's a nice day")        
+elif temperature >20 or temperature <=30:
+    print("It's a bit cold")    
+elif not(temperature >0 and temperature <=10):
+    print("It's cold")
+else:
+    print("It's freezing cold")
